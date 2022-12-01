7053c47cb400a6f9cfba94d4568619960ef49509
Dylan Worseling <dylan.worseling@hva.nl>
2022-12-01 11:24:07 +0100

    fix: directory issues and spelling

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,5 @@
 /**
- * @author Dylan Worsling
+ * @author Dylan Worseling
  */
 
 import java.io.File;
@@ -15,23 +15,22 @@ public class Day1 {
         int elfTotal = 0;
         int arrGrootte = 0;
 
-
         try {
             File myObj = new File("input/day1.txt");
             Scanner myReader = new Scanner(myObj);
 
             while (myReader.hasNextLine()) {
                 data = myReader.nextLine();
-                //System.out.println(data);
                 if(data == "") {
                     myReader.nextLine();
                     arrGrootte++;
                 }
             }
+
             myReader.close();
             myReader = new Scanner(myObj);
 
-            System.out.println("Array: " + arrGrootte);
+            System.out.println("\nArray grootte: " + arrGrootte);
             int elf[] = new int[arrGrootte];
 
             for (int i = 0; i < arrGrootte;) {
@@ -45,13 +44,13 @@ public class Day1 {
             }
 
             Arrays.sort(elf);
-            System.out.println("Elf met meeste calorieën: " + elf[arrGrootte - 1]);
+            System.out.println("\nElf met meeste calorieën: " + elf[arrGrootte - 1]);
 
             for (int i = 1; i < 4; i++) {
                 elfTotal += elf[arrGrootte - i];
             }
 
-            System.out.println("Top 3 totaal: " + elfTotal);
+            System.out.println("\nTop 3 totale calorieën: " + elfTotal);
 
 
         } catch (FileNotFoundException e) {
